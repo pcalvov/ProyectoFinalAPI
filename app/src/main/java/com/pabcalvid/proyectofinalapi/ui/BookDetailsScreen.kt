54f9180101f949d808072164ec0b6ad7f56615c0
Pablo <pablocv0805@gmail.com>
2025-02-24 13:18:31 +0100

    Scroll añadido

--- app/src/main/java/com/pabcalvid/proyectofinalapi/ui/BookDetailsScreen.kt
+++ app/src/main/java/com/pabcalvid/proyectofinalapi/ui/BookDetailsScreen.kt
@@ -1,10 +1,13 @@
 package com.pabcalvid.proyectofinalapi.ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.pabcalvid.proyectofinalapi.data.local.Book
@@ -12,21 +15,28 @@ import com.pabcalvid.proyectofinalapi.data.local.Book
 @Composable
 fun BookDetailsScreen(book: Book, onFavoriteClick: (Book) -> Unit) {
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(book.title, style = MaterialTheme.typography.headlineMedium)
+        Text(book.title, style = MaterialTheme.typography.headlineMedium, textAlign = TextAlign.Center)
         Spacer(modifier = Modifier.height(16.dp))
 
-        AsyncImage(model = book.cover, contentDescription = "Portada de ${book.title}", modifier = Modifier.size(200.dp))
+        AsyncImage(
+            model = book.cover,
+            contentDescription = "Portada de ${book.title}",
+            modifier = Modifier.size(200.dp)
+        )
         Spacer(modifier = Modifier.height(16.dp))
 
         Text("Título Original: ${book.originalTitle}", style = MaterialTheme.typography.bodyLarge)
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
         Text("Fecha de Publicación: ${book.releaseDate}", style = MaterialTheme.typography.bodyLarge)
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
         Text("Descripción: ${book.description}", style = MaterialTheme.typography.bodyLarge)
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
         Text("Número de Páginas: ${book.pages}", style = MaterialTheme.typography.bodyLarge)
 
         Spacer(modifier = Modifier.height(16.dp))
